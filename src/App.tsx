41bf9559e13300ae20e3232a598161da8d3c9368
Supriyo Ghorui <supriyoghorui645@gmail.com>
2026-01-16 14:49:51 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import { LoginPage } from './features/auth/LoginPage';
 import { HomePage } from './features/dashboard/HomePage';
 import ProtectedRoute from './layouts/ProtectedRoute';
@@ -8,7 +6,6 @@ import ProtectedRoute from './layouts/ProtectedRoute';
 
 function App() {
   return (
-    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
@@ -23,7 +20,6 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
-    </Provider>
   );
 }
 
